eb5d85159b90b0e79f67df551586754d0f178f8f
Khairul Akmal <khairulakmal.dev@gmail.com>
2022-05-05 15:24:25 +0800

    feat: implement helper method to read id param

---
+++ backend/cmd/api/helpers.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil {
+		return 0, errors.New("invalid ID parameter")
+	}
+
+	return id, nil
+}
